e15e541069fb5a541addcbaad01b88324c236aec
Bhawna0101 <Bhawnaarora0101@gmail.com>
2019-11-01 13:28:13 +0530

    message 2

--- src/main/java/com/crm/qa/utilities/ReadDataFromExcel.java
+++ src/main/java/com/crm/qa/utilities/ReadDataFromExcel.java
@@ -3,11 +3,18 @@ package com.crm.qa.utilities;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import net.sf.jxls.reader.XLSReadStatus;
+import net.sf.jxls.reader.XLSReader;
+import net.sf.jxls.reader.XLSSheetReader;
 
 public class ReadDataFromExcel {
 
@@ -15,7 +22,11 @@ public class ReadDataFromExcel {
 		File filename = new File("D://EclipseWorksapce//MyFrameworkArt//src//main//java//com//crm//qa//testdata//Worksheet.xlsx");
 
 		FileInputStream fis = new FileInputStream(filename);
-		Workbook TestWorkbook = new XSSFWorkbook(fis);
+			
+			
+		
+		XSSFWorkbook  TestWorkbook = new XSSFWorkbook(fis);
+		//Workbook TestWorkbook = new XSSFWorkbook(fis);
 		//String fileExtensionName = filename.substring("Worksheet.xlsx".indexOf("."));
 		//System.out.println(fileExtensionName);
 
@@ -23,6 +34,7 @@ public class ReadDataFromExcel {
 		
 		Sheet testSheet = TestWorkbook.getSheet("Sheet1");
 		int rowcount = testSheet.getLastRowNum()-testSheet.getFirstRowNum();
+		System.out.println(testSheet.getLastRowNum()+rowcount);
 		for (int i = 0; i < rowcount+1; i++) {
 
 	        Row row = testSheet.getRow(i);
